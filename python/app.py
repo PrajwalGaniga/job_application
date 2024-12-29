26cff1f08b3aa67c33911f6530dbf38a91e2cdac
PrajwalGaniga <prajwalganiga06@gmail.com>
2024-12-30 00:16:24 +0530

    Add files via upload

---
+++ python/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
+
+JOBS=[
+    {"id": 1, "title": "Software Engineer", "location": "New York, NY", "salary": "$120,000/year"},
+     {"id": 4, "title": "UX/UI Designer", "location": "Seattle, WA", "salary": "$110,000/year"},
+    {"id": 2, "title": "Product Manager", "location": "San Francisco, CA", "salary": "$140,000/year"},
+    {"id": 3, "title": "Data Analyst", "location": "Austin, TX", "salary": "$90,000/year"},
+    {"id": 4, "title": "UX/UI Designer", "location": "Seattle, WA", "salary": "$110,000/year"},
+    {"id": 5, "title": "DevOps Engineer", "location": "Boston, MA", "salary": "$125,000/year"}
+]
+
+@app.route('/')
+def home():
+    return render_template('home.html', jobs=JOBS,Company='TastinGo')
+
+
+@app.route('/api/jobs')
+def job_list():
+    return jsonify(JOBS)
+
+
+if __name__=="__main__":
+    app.run(host='0.0.0.0',debug=True)
